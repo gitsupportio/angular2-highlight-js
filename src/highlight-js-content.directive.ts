9b213b283a98e8c201f75dd3de0ff1b07eb64a87
Brandon <brandonyoyoslade@gmail.com>
2017-05-20 18:29:23 -0600

    fix(directive): only call hljs.highlightBlock if innerHtml has changed since the last call

--- src/highlight-js-content.directive.ts
+++ src/highlight-js-content.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit, OnDestroy, AfterViewChecked } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, AfterViewChecked } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -7,50 +7,35 @@ declare var hljs: any;
 @Directive({
     selector: '[highlight-js-content]'
 })
-export class HighlightJsContentDirective implements OnInit, OnDestroy, AfterViewChecked {
+export class HighlightJsContentDirective implements OnInit, AfterViewChecked {
     constructor(private elementRef: ElementRef) { }
 
     @Input() useBr: boolean;
     @Input('highlight-js-content') highlightSelector: string;
-
-    private allowChange: boolean = true;
-    private _subscription: Subscription | null;
-    @Input()
-    set changes(val: Observable<void> | null) {
-        if (this._subscription) {
-            this._subscription.unsubscribe();
-            this._subscription = null;
-        }
-        if (val) {
-            this._subscription = val.subscribe(() => this.allowChange = true);
-        }
-    }
+    private previousInnerHtml: string | null = null;
 
     ngOnInit() {
         if (this.useBr) {
             hljs.configure({ useBR: true });
         }
     }
-    ngOnDestroy() {
-        if (this._subscription) {
-            this._subscription.unsubscribe();
-            this._subscription = null;
-        }
-    }
 
     ngAfterViewChecked() {
         let selector = this.highlightSelector || 'code';
 
-        if (this.elementRef.nativeElement.innerHTML) {
-            if (!this.allowChange) return;
-            this.allowChange = false;
+        let innerHtml = this.elementRef.nativeElement.innerHTML;
+        if (innerHtml === this.previousInnerHtml) return;
+        this.previousInnerHtml = innerHtml;
 
+        if (innerHtml) {
             let snippets = this.elementRef.nativeElement.querySelectorAll(selector);
-            
+
             for(var snippet of snippets) {
-                console.log("Updating highlights!");
                 hljs.highlightBlock(snippet);
-            } 
+            }
+
+            innerHtml = this.elementRef.nativeElement.innerHTML;
+            this.previousInnerHtml = innerHtml;
         }
     }
 }
